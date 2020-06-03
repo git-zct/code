fb266a4d55b12af5536807d398aed84948f00f1c
git-zct <63958208+git-zct@users.noreply.github.com>
2020-06-03 21:42:51 +0800

    看看写得多烂

---
+++ 6.3/7. 整数反转.c
@@ -0,0 +1,42 @@
+int reverse(int x){
+    if(!x){
+        return 0;
+    }
+    int flag=0;
+    long temp=0;
+  if(x<0){
+      if(x==-2147483648)
+      x=-x;
+      flag=1;
+  }
+    while(x){
+        temp*=10;
+        temp+=x%10;
+        x/=10;
+    }
+    if(temp!=(int)temp){
+        return 0;
+    }
+    if(flag){
+        return -(int)temp;
+    }else{
+        return (int)temp;
+    }
+}
+// #define isOverLength 0
+
+// int reverse(int x){
+//     long lRet = 0;
+//     while(0 != x)
+//     {
+//         lRet = lRet * 10 + x % 10;
+//         x = x / 10;
+//     }
+
+//     if((int)lRet != lRet)
+//     {
+//         return isOverLength;
+//     }
+
+//     return (int)lRet;
+// }
